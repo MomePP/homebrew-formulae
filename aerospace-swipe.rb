65c1a10f5173145a2e1fb75b8d0b957dfc6baada
momeppkt <peeranut32@gmail.com>
2026-07-08 14:20:30 +0700

    aerospace-swipe: update to v1.1.1

--- aerospace-swipe.rb
+++ aerospace-swipe.rb
@@ -7,8 +7,8 @@ class AerospaceSwipe < Formula
   desc "Trackpad swipe gestures for AeroSpace workspace switching"
   homepage "https://github.com/MomePP/aerospace-swipe"
   url "https://github.com/MomePP/aerospace-swipe.git",
-      tag:      "v1.1.0",
-      revision: "23fd2e23a4f5d05e4c0c2be7e98e99f2c064b5ca"
+      tag:      "v1.1.1",
+      revision: "7d468e93bead8d71bfea8ca993cc04e53fb76dab"
   license "MIT"
   head "https://github.com/MomePP/aerospace-swipe.git", branch: "main"
 
